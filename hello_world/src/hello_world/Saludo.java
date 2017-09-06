b0a51e1589316283205596edd8d8482636192549
margamba <31647313+margamba@users.noreply.github.com>
2017-09-06 17:06:10 +0200

    Update Saludo.java
    commit en github

--- hello_world/src/hello_world/Saludo.java
+++ hello_world/src/hello_world/Saludo.java
@@ -1,9 +1,9 @@
 package hello_world;
 
 public class Saludo {
-	
+	private String nombre="Pepe"
 	public static void main(String[] args) {
-		System.out.println("hola mundo");
+		System.out.println("hola mundo "+ nombre);
 	}
 
 }
