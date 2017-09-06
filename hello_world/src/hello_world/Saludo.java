8316b5605e417e3624af7a44cbeb8ba27d0f335d
mgamba <mgamba@ATMGAMBAP.atsistemas.lan>
2017-09-06 17:24:20 +0200

    nuevo branch

--- hello_world/src/hello_world/Saludo.java
+++ hello_world/src/hello_world/Saludo.java
@@ -1,9 +1,17 @@
 package hello_world;
 
 public class Saludo {
-	private static String nombre="Pepe";
+	private static String nombre="Pepes";
 	public static void main(String[] args) {
-		System.out.println("hola mundo "+ nombre);
+		System.out.println("hola  "+ getNombre());
+	}
+	
+	
+	public static String getNombre() {
+		return nombre;
+	}
+	public static void setNombre(String nombre) {
+		Saludo.nombre = nombre;
 	}
 
 }
