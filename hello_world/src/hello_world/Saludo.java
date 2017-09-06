2c158321c5e3efa1d9bc7f10aaade3727736440d
mgamba <mgamba@ATMGAMBAP.atsistemas.lan>
2017-09-06 17:08:53 +0200

    segunda subida

--- hello_world/src/hello_world/Saludo.java
+++ hello_world/src/hello_world/Saludo.java
@@ -1,7 +1,7 @@
 package hello_world;
 
 public class Saludo {
-	private String nombre="Pepe"
+	private static String nombre="Pepe";
 	public static void main(String[] args) {
 		System.out.println("hola mundo "+ nombre);
 	}
